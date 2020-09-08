67bfd73ed1d9a10591b7406e6584b12b94721249
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-09-08 10:33:38 +0100

    Initial Commit

---
+++ SpaceshipOperator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpaceshipOperator
+//
+//  Created by Gary Watson on 08/09/2020.
+//  Copyright © 2020 Gary Watson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
